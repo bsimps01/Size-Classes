f3c5dc5cab9fae830a8cb1153cc60670ecfd9a53
Ben Simpson <b.albertsimpson@gmail.com>
2020-02-03 10:11:04 -0800

    added end result to Size classes file

---
+++ Size Classes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Size Classes
+//
+//  Created by Benjamin Simpson on 1/30/20.
+//  Copyright © 2020 Benjamin Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
